47015492d2b2b025134935b41ae6c7003e168815
Allan Bogh <ajbogh@allanbogh.com>
2020-06-08 22:43:30 -0700

    Adjusted keyword offline recognition and command online recognition

--- lib/command_interpreter.py
+++ lib/command_interpreter.py
@@ -10,25 +10,26 @@ MIN_DISTANCE = 5
 def interpret_command(utterance):
   closest_command_index = -1
   closest_dist = sys.maxsize
-  with open('./config/commands.json') as command_file:
-    data = json.load(command_file)
-    if data['keyword'] not in utterance: 
-      return 0
-    for index, p in enumerate(data['commands']):
-      dist1 = distance('{} {}'.format(data['keyword'], p['utterance']), utterance)
-      dist2 = distance('{} {}'.format(p['utterance'], data['keyword']), utterance)
-      utterance_dist = min( dist1, dist2 )
-      if utterance_dist < closest_dist:
-        closest_command_index = index
-        closest_dist = utterance_dist
-      
-      # print('Subprocess options: '+p['subprocess_options'])
-    if closest_dist > -1 and closest_dist < MIN_DISTANCE:
-      command = data['commands'][closest_command_index]
-      print('Closest command:', command['utterance'])
-      print('Interpreted utterance: ' + utterance)
-      print('Utterance: ' + command['utterance'])
-      print('Command: ' + command['command'])
-      return execute_command(command['command'])
-    else:
-      return 0
+  with open('./config/config.json') as config_file:
+    config = json.load(config_file)
+    with open('./config/commands.json') as command_file:
+      commands = json.load(command_file)
+      if config['keyword'] not in utterance: 
+        return 0
+      for index, p in enumerate(commands):
+        utterance_dist1 = distance('{} {}'.format(config['keyword'], p['utterance']), utterance)
+        utterance_dist2 = distance(p['utterance'], utterance)
+        utterance_dist = min(utterance_dist1, utterance_dist2)
+        if utterance_dist < closest_dist:
+          closest_command_index = index
+          closest_dist = utterance_dist
+
+      if closest_dist > -1 and closest_dist < MIN_DISTANCE:
+        command = commands[closest_command_index]
+        print('Closest command:', command['utterance'])
+        print('Interpreted utterance: ' + utterance)
+        print('Utterance: ' + command['utterance'])
+        print('Command: ' + command['command'])
+        return execute_command(command['command'])
+      else:
+        return 0
--- stt.py
+++ stt.py
@@ -5,6 +5,10 @@ import os
 import time
 from lib.command_interpreter import interpret_command
 import json
+import subprocess
+from Levenshtein import distance
+
+KEYWORD_THRESHOLD = 5
 
 r = sr.Recognizer()
 mic = sr.Microphone()
@@ -13,6 +17,7 @@ old_energy_threshold = -1
   
 def listen(source):  
   global old_energy_threshold
+  global KEYWORD_THRESHOLD
   print('Listening...')
   audio = r.listen(source)
 
@@ -24,13 +29,23 @@ def listen(source):
       config = json.load(config_file)
       client_id = config['houndify']['client_id']
       client_key = config['houndify']['client_key']
-      utterance = r.recognize_houndify(audio, client_id, client_key)
-      # utterance = r.recognize_sphinx(audio)
-      old_energy_threshold = r.energy_threshold
-      r.energy_threshold = 10000
-      print("Recognized:", utterance)
-      print("Updated energy threshold from {} to {}".format(old_energy_threshold, r.energy_threshold))
-      interpret_command(utterance)
+      utterance = r.recognize_sphinx(audio)
+      keyword_utterance = utterance.split(' ', 2)
+      if (
+        distance(keyword_utterance[0], config['keyword']) < KEYWORD_THRESHOLD or 
+        (len(keyword_utterance) > 1 and distance('{} {}'.format(keyword_utterance[0], keyword_utterance[1]), config['keyword']) < KEYWORD_THRESHOLD)
+      ):
+        print("Keyword detected: spoken '{}', keyword '{}'".format(utterance, config['keyword']))
+        old_energy_threshold = r.energy_threshold
+        print("Analyzing command...")
+        utterance = r.recognize_houndify(audio, client_id, client_key)
+        old_energy_threshold = r.energy_threshold
+        r.energy_threshold = 10000
+        print("Recognized:", utterance)
+        print("Updated energy threshold from {} to {}".format(old_energy_threshold, r.energy_threshold))
+        interpret_command(utterance)
+      else: 
+        print("Keyword check recognized: '{}', not '{}'".format(utterance, config['keyword']))
   except sr.RequestError as e:
     # API was unreachable or unresponsive
     print("Error: API unavailable", e)
